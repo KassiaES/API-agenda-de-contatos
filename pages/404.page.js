a49c83e4c69c18ece9ae2c26fbede0460b8c4b59
kdinizf <karina.dinizf@gmail.com>
2023-04-02 03:30:47 -0300

    +ajuste

--- pages/404.page.js
+++ pages/404.page.js
@@ -3,7 +3,6 @@ notFound.setAttribute('id', 'p-not-found')
 
 export const NotFound = () => {
     notFound.innerHTML = `      
-        
     <img src = "https://weeblytutorials.com/wp-content/uploads/2017/05/Weebly-404-Page-Not-Found-Error1.png"/>
     <button type="submit"><a href="#login">Voltar a página de login</a></button>
     `
